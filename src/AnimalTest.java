eea48fa6340261f9b6515a835b436a6aa11fec15
Ry Fleming <ry.fleming@ou.edu>
2019-02-09 21:53:40 -0600

    implement fullConstructorTest() and toStringTest()

--- src/AnimalTest.java
+++ src/AnimalTest.java
@@ -23,5 +23,43 @@ public class AnimalTest
     }
 
     // TODO: test full constructor, getters, and toString
+    
+    public void fullConstructorTest() throws AssertException
+    {
+    	
+    	// Use full constructor with arbitrary, but appropriate, values for parameters
+    	String color = "color";
+    	String name = "name";
+    	double height = 10.0;
+    	double weight = 10.0;
+    	
+    	Animal animal = new Animal(color, name, height, weight);
+    	
+    	// The name should be "name", color "color", height and weight 10.
+    	Assert.assertEquals("color", animal.getColor());
+    	Assert.assertEquals("name", animal.getName());
+    	Assert.assertEquals(10.0, animal.getHeight());
+    	Assert.assertEquals(10.0, animal.getWeight());
+    	
+    }
+    
+    public void toStringTest() throws AssertException
+    {
+    	Animal animal = new Animal("blue", "Jeff", 10.0, 100.0);
+    	String actual = animal.toString();
+    	String expected = "Jeff, a blue-colored animal. 100.0 pounds, 10.0 inches.\n";
+    	
+    	boolean isCorrect = false;
+    	
+    	if(actual.equals(expected))
+    	{
+    		isCorrect = true;
+    	}
+    	
+    	Assert.assertTrue(isCorrect);
+    	
+    	
+    	
+    }
 }
 
