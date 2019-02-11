bcf5fce21b2d87eaeb3ba01ff90b5f2f931a3cb9
Ry Fleming <ry.fleming@ou.edu>
2019-02-11 17:20:49 -0600

    add second animal to class AnimalTest

--- src/AnimalTest.java
+++ src/AnimalTest.java
@@ -15,7 +15,8 @@ public class AnimalTest
      */
     public void emptyConstructorTest() throws AssertException
     {
-        // Use the default constructor
+        
+    	// Use the default constructor
         Animal animal = new Animal();
 
         // The name should be "noname", color "unknown", height and weight 0.
@@ -23,63 +24,92 @@ public class AnimalTest
         Assert.assertEquals("noname", animal.getName());
         Assert.assertEquals(0, animal.getHeight(), 0.01);
         Assert.assertEquals(0, animal.getWeight(), 0.01);
+        
     }
 
     /**
-     * Test full Animal constructor and getters
+     * Test full Animal constructor and getters, with two different animals
      * @throws AssertException
      */
     
     public void fullConstructorTest() throws AssertException
     {
-    	
+    	//Test 1
     	// Use full constructor with arbitrary, but appropriate, values for parameters
-    	String color = "color";
-    	String name = "name";
-    	double height = 10.0;
-    	double weight = 10.0;
+    	String colorA = "color";
+    	String nameA = "name";
+    	double heightA = 10.0;
+    	double weightA = 10.0;
     	
-    	Animal animal = new Animal(color, name, height, weight);
+    	Animal animalA = new Animal(colorA, nameA, heightA, weightA);
     	
     	// The name should be "name", color "color", height and weight 10.
-    	Assert.assertEquals("color", animal.getColor());
-    	Assert.assertEquals("name", animal.getName());
-    	Assert.assertEquals(10.0, animal.getHeight(), .01);
-    	Assert.assertEquals(10.0, animal.getWeight(), .01);
+    	Assert.assertEquals("color", animalA.getColor());
+    	Assert.assertEquals("name", animalA.getName());
+    	Assert.assertEquals(10.0, animalA.getHeight(), .01);
+    	Assert.assertEquals(10.0, animalA.getWeight(), .01);
     	
+    		//Test 1
+    		// Use full constructor with arbitrary, but appropriate, values for parameters
+    		String colorB = "Blue";
+    		String nameB = "Jeff";
+    		double heightB = 100.0;
+    		double weightB = 100.0;
+    	
+    		Animal animalB = new Animal(colorB, nameB, heightB, weightB);
+    	
+    		// The name should be "name", color "color", height and weight 10.
+    		Assert.assertEquals("Blue", animalB.getColor());
+    		Assert.assertEquals("Jeff", animalB.getName());
+    		Assert.assertEquals(100.0, animalB.getHeight(), .01);
+    		Assert.assertEquals(100.0, animalB.getWeight(), .01);
     }
     
     /**
-     * Test Animal toString method
+     * Test Animal toString method, with two different animals
      * @throws AssertException
      */
     public void toStringTest() throws AssertException
     {
-    	String expted = "Jeff, a blue-colored animal. 100.0 pounds, 10.0 inches\n";
-    	Animal animal = new Animal("blue", "Jeff", 100.0, 10.0);
-    	String actl = animal.toString();
-    	Assert.assertEquals(expted, actl);
+    	//Test1
+    	//Use arbitrary, but appropriate, string
+    	String exptedA = "name, a color-colored animal. 10.0 pounds, 10.0 inches\n";
+    	Animal animalA = new Animal("color", "name", 10.0, 10.0);
+    	String actlA = animalA.toString();
+    	Assert.assertEquals(exptedA, actlA);
+    	
+    		//Test2
+    		//Use arbitrary, but appropriate, string
+    		String exptedB = "Jeff, a Blue-colored animal. 100.0 pounds, 100.0 inches\n";
+    		Animal animalB = new Animal("Blue", "Jeff", 100.0, 100.0);
+    		String actlB = animalB.toString();
+    		Assert.assertEquals(exptedB, actlB);
     	
     }
-    
+    /**
+     * Test Animal equals() method, with two different animals
+     * @throws AssertException
+     */
     public void equalsTest() throws AssertException
     {
-    	String color = "color";
-    	String name = "name";
-    	double height = 10.0;
-    	double weight = 10.0;
     	
-    	String strg = "color, name, 10.0, 10.0";
+    	//Test 1
+    	String colorA = "color";
+    	String nameA = "name";
+    	double heightA = 10.0;
+    	double weightA = 10.0;
     	
-    	Animal testA = new Animal(color, name, height, weight);
-    	Animal testB = new Animal(color, name, height, weight);
+    	String strgA = "color, name, 10.0, 10.0";
+    	
+    	Animal testA = new Animal(colorA, nameA, heightA, weightA);
+    	Animal testB = new Animal(colorA, nameA, heightA, weightA);
     			
     			
     	//Test same-object condition of method
     	Assert.assertTrue(testA.equals(testB));
     			
     	//Test not-an-Animal condition of method
-    	Assert.assertFalse(testA.equals(strg));
+    	Assert.assertFalse(testA.equals(strgA));
     			
     	//Test all-fields-match condition of method
     	Assert.assertEquals("color", testA.getColor());
@@ -87,7 +117,29 @@ public class AnimalTest
     	Assert.assertEquals(10.0, testA.getHeight(), .01);
     	Assert.assertEquals(10.0, testA.getWeight(), .01);
     	
+    		//Test 1
+    		String colorB = "Blue";
+    		String nameB = "Jeff";
+    		double heightB = 100.0;
+    		double weightB = 100.0;
+    	
+    		String strgB = "Blue, Jeff, 100.0, 100.0";
+    	
+    		Animal testC = new Animal(colorB, nameB, heightB, weightB);
+    		Animal testD = new Animal(colorB, nameB, heightB, weightB);
     			
+    			
+    		//Test same-object condition of method
+    		Assert.assertTrue(testC.equals(testD));
+    			
+    		//Test not-an-Animal condition of method
+    		Assert.assertFalse(testC.equals(strgB));
+    			
+    		//Test all-fields-match condition of method
+    		Assert.assertEquals("Blue", testC.getColor());
+    		Assert.assertEquals("Jeff", testC.getName());
+    		Assert.assertEquals(100.0, testC.getHeight(), .01);
+    		Assert.assertEquals(100.0, testC.getWeight(), .01);		
     }
     
 }
